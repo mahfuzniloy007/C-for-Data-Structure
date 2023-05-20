7bf02e61fdcef406ec65e2689d10a2df1118ac15
Mahfuz <maniscrazy76@gmail.com>
2023-05-21 01:07:14 +0600

    1st commit

---
+++ Week 1/Module 1/min_max.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int mn=min(a,b);
+    int mx=max(a,b);
+    cout<<mn<<endl<<mx;
+}
---
+++ Week 1/Module 1/output.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // std::cout<<"Hello World";
+    //for printing new line
+    // std::cout<<"Hello "<<std::endl<<"World"; -> Method 1
+    // std::cout<<"Hello\n"<<"World\n"; -> Method 2
+    int a=100,b=200;
+    cout<<a<<endl<<b;
+    return 0;
+}
