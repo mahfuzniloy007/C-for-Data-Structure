92aed1ce5ddf00dda2553f31fc838379bdfb97e5
Mahfuz <maniscrazy76@gmail.com>
2023-05-22 17:33:10 +0600

    1st commit

---
+++ Week 1/Module 2/dynamic_data.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int *a = new int;
+    *a = 100;
+    cout << *a << endl;
+    float *f = new float;
+    *f = 300;
+    cout << *f << endl;
+    return 0;
+}
---
+++ Week 1/Module 2/ternary_operator.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a = 10;
+    // if (a%2==0)
+    // {
+    //     cout<<"Even";
+    // }
+    // else
+    // {
+    //     cout<<"Odd";
+    // }
+    (a % 2 == 0) ? cout << "Even" : cout << "Odd";
+    return 0;
+}
